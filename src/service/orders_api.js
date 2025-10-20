fd0fb3cc7c8c4b3d2a1ba57ca0fad70caf4fe515
unknown <92609@sanmin.com>
2025-10-20 10:29:19 +0800

    update url api order

--- src/service/orders_api.js
+++ src/service/orders_api.js
@@ -1,12 +1,12 @@
-const base_url = "https://localhost:7145/api/"
+const base_url = "https://localhost:7145/api/order/"
 
 export async function getOrder() {
-    const res = await fetch(`${base_url}order`)
+    const res = await fetch(`${base_url}get`)
     return res.json();
 }
 
 export async function createOrder(data) {
-    const res = await fetch(`${base_url}create_order`, {
+    const res = await fetch(`${base_url}create`, {
         method: "POST",
         headers: {
             'Content-Type': 'application/json'
@@ -17,7 +17,7 @@ export async function createOrder(data) {
 }
 
 export async function updateOrder(data) {
-    const res = await fetch(`${base_url}update_order`, {
+    const res = await fetch(`${base_url}update`, {
         method: "PUT",
         headers: {
             'Content-Type': 'application/json'
@@ -28,7 +28,7 @@ export async function updateOrder(data) {
 }
 
 export async function delOrder(data) {
-    const res = await fetch(`${base_url}del_order`, {
+    const res = await fetch(`${base_url}del`, {
         method: "Delete",
         headers: {
             'Content-Type': 'application/json'
