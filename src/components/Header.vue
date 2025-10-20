95e3b3f3069a667917025ba708243d31de54ebd6
unknown <92609@sanmin.com>
2025-10-20 11:30:04 +0800

    fix header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,9 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <div class="Title fs-4">
+                <router-link to='/Index'>Index</router-link>
+            </div>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -13,9 +15,6 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item router">
-                        <router-link to="/Order">Order</router-link>
-                    </li>
-                    <li class="nav-item">
                         <router-link to="/Order">Order</router-link>
                     </li>
                     <li class="nav-item">
@@ -27,4 +26,12 @@
     </nav>
 </template>
 
-<style scoped></style>
+<style scoped>
+.Title {
+    margin: 5px 20px 5px 10px;
+}
+
+.nav-item {
+    margin: 0 5px;
+}
+</style>
--- src/components/Table_List.vue
+++ src/components/Table_List.vue
@@ -14,23 +14,21 @@ const props = defineProps({
 </script>
 
 <template>
-  <div>
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th v-for="(col, i) in columns" :key="i">{{ col.label }}</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(row, i) in data" :key="i" @click="$emit('rowClick', row)">
-          <td v-for="(col, j) in columns" :key="j">{{ row[col.field] }}</td>
-        </tr>
-      </tbody>
-    </table>
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th v-for="(col, i) in columns" :key="i">{{ col.label }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(row, i) in data" :key="i" @click="$emit('rowClick', row)">
+        <td v-for="(col, j) in columns" :key="j">{{ row[col.field] }}</td>
+      </tr>
+    </tbody>
+  </table>
 
-    <div v-if="loading">Loading...</div>
-    <div v-else-if="!data.length">No data available</div>
-  </div>
+  <div v-if="loading">Loading...</div>
+  <div v-else-if="!data.length">No data available</div>
 </template>
 
 <style scoped></style>
