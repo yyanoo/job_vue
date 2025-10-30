9699218d1222ce96bda88b92065e00b9331874f1
unknown <92609@sanmin.com>
2025-10-30 11:18:26 +0800

    need fix ui

--- src/components/Order/Create_Order_Model.vue
+++ src/components/Order/Create_Order_Model.vue
@@ -16,30 +16,18 @@ const props = defineProps({
 
 const arr = reactive([])
 
-const orderid = ref('')
 const productid = ref('')
 const qty = ref()
 const discount = ref()
-const is_order = ref(false)
 
-//新增 order list 要 res 到後端時將 list 一次送出到後端建立 data
 const add_item = async () => {
     //檢查 input 是否 有 data
-    if (orderid.value && productid.value && qty.value && discount.value) {
-
+    if (productid.value && qty.value && discount.value) {
         //確認 discount 和 qty 是否超過 或小於一般 數值
         if (discount.value > 100 || discount.value < 1 || qty.value < 1) {
             return alert("請輸入 Qty 和 Discount");
         }
 
-        //呼叫api 檢查 order
-        await use_check.check_order(orderid.value)
-        if (use_check.check_bool.data == false) {
-            is_order.value = true
-        } else if (use_check.check_bool.data == true) {
-            return alert("OrderID 已被建立")
-        }
-        
         //檢查 List 是否有相同 productid
         const check = arr.some(item => item.ProductID === productid.value)
         if (check) {
@@ -54,7 +42,6 @@ const add_item = async () => {
 
         // 將資料加入陣列
         arr.push({
-            OrderID: orderid.value,
             ProductID: productid.value,
             Qty: qty.value,
             Discount: discount.value
@@ -100,18 +87,7 @@ const del_item = (index) => {
             <!-- Model Header -->
             <div class="modal-content" style="transform: translateY(100px);">
                 <div class="modal-header" style="margin-top: 10px;">
-                    <h1 class="modal-title fs-4" id="createOrderLabel" style="margin-left: 10px;">OrderID :</h1>
-
-                    <!-- OrderID .value -->
-                    <!-- 未輸入 OrderID -->
-                    <div class="" v-if="!is_order">
-                        <input type="text" placeholder="OrderID" v-model="orderid">
-                    </div>
-                    <!-- 已輸入 OrderID -->
-                    <div class="" v-if="is_order">
-                        <div class="fs-4" style="margin-left: 10px;">{{ orderid }}</div>
-                    </div>
-
+                    <h1 class="modal-title fs-4" id="createOrderLabel" style="margin-left: 10px;">Order Create List</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
                         style="transform: translateY(-15px);"></button>
                 </div>
@@ -119,33 +95,6 @@ const del_item = (index) => {
                 <!-- Modal Body -->
                 <div class="modal-body">
                     <div class="d-flex flex-column ">
-
-                        <!-- 將 list資料 列出來 能夠響應式修改 list資料 -->
-                        <div v-for="(item, index) in arr" :key="index" class="d-flex justify-content-center"
-                            style="margin-bottom: 10px;">
-                            <div class="box">
-                                <div class="">ProductID :</div>
-                                <!-- Product .value -->
-                                <div class="" style="margin-left: 10px; margin-right: 10px;">{{ item.ProductID }}</div>
-                            </div>
-                            <div class="box">
-                                <div class="">Qty :</div>
-                                <!-- Qty .value -->
-                                <input type="number" class="input-box" v-model="item.Qty">
-                                <div class="">Discount :</div>
-                                <!-- Discount .value -->
-                                <input type="number" class="input-box" v-model="item.Discount">
-                                <!-- 刪除 list 對應 index 按鈕 -->
-                                <button type="button" class="btn btn-primary" style="margin-left: 20px;"
-                                    @click="del_item(index)">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                        class="bi bi-trash3" viewBox="0 0 16 16">
-                                        <path
-                                            d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
-                                    </svg>
-                                </button>
-                            </div>
-                        </div>
 
                         <!-- 將資料存到 ref 並且送出到 list -->
                         <div class="d-flex justify-content-center">
@@ -172,6 +121,34 @@ const del_item = (index) => {
                                 </button>
                             </div>
                         </div>
+
+                        <!-- 將 list資料 列出來 能夠響應式修改 list資料 -->
+                        <div v-for="(item, index) in arr" :key="index" class="d-flex justify-content-center"
+                            style="margin-top: 10px;">
+                            <div class="box">
+                                <div class="">ProductID :</div>
+                                <!-- Product .value -->
+                                <div class="" style="margin-left: 10px; margin-right: 10px;">{{ item.ProductID }}</div>
+                            </div>
+                            <div class="box">
+                                <div class="">Qty :</div>
+                                <!-- Qty .value -->
+                                <input type="number" class="input-box" v-model="item.Qty">
+                                <div class="">Discount :</div>
+                                <!-- Discount .value -->
+                                <input type="number" class="input-box" v-model="item.Discount">
+                                <!-- 刪除 list 對應 index 按鈕 -->
+                                <button type="button" class="btn btn-primary" style="margin-left: 20px;"
+                                    @click="del_item(index)">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        class="bi bi-trash3" viewBox="0 0 16 16">
+                                        <path
+                                            d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
 
